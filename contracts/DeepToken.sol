4582411113ad2bb37db72e89512483ee252a89bb
DeepakSingh260 <alexmha260@gmail.com>
2023-03-20 12:41:02 +0530

    token delegation feature added

--- contracts/DeepToken.sol
+++ contracts/DeepToken.sol
@@ -11,8 +11,9 @@ contract DeepToken{
     event Transfer(address indexed _from , address indexed _to , uint256 _value);
 
     event Approve(address indexed _owner , address indexed _spender , uint256 _value);
-
     mapping(address => uint256) public balance_of;
+    mapping(address => mapping(address=>uint256)) public allowance;
+
     constructor(uint256 _initialSupply){
         balance_of[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
@@ -29,8 +30,21 @@ contract DeepToken{
 
     function approve(address _spender , uint256 _value)public returns (bool sucess){
 
+        allowance[msg.sender][_spender]=_value;
         emit Approve(msg.sender, _spender, _value);
+
         return true;
+    }
+
+    function transferFrom(address _from , address _to , uint256 _value) public returns (bool sucess){
+            require(balance_of[_from]>= _value);
+            require(allowance[_from][msg.sender]>= _value);
+            
+            balance_of[_from]-=_value;
+            balance_of[_to]+=_value;
+            allowance[_from][msg.sender]-=_value;
+            emit Transfer(_from , _to , _value);
+            return true;
     }
     
 }
--- test/deepToken.js
+++ test/deepToken.js
@@ -2,6 +2,9 @@ var DeepToken = artifacts.require('DeepToken')
 
 contract(DeepToken , function(accounts){
     var tokenInstance;
+    var fromAccount;
+    var toAccount;
+    var spendingAccount;
     it('check if the token is  initialised correctly',()=>{
         return DeepToken.deployed().then((instance)=>{
             tokenInstance = instance;
@@ -66,7 +69,54 @@ contract(DeepToken , function(accounts){
             assert(reciept.logs[0].args._spender,accounts[1],'logs the account the tokens are authorized to')
             assert(reciept.logs[0].args._value,100,'logs the  token value authorized')
             
-           
+           return tokenInstance.allowance(accounts[0] , accounts[1])
+
+        }).then((allowance)=>{
+            assert.equal(Number(allowance),100 , 'stores the allowance of delegated transfer')
         })
     });
+
+    it('handle delegate token transfers' , ()=>{
+        
+        return DeepToken.deployed().then((instance)=>{
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+
+            return tokenInstance.transfer(fromAccount , 100 , {from:accounts[0]})
+
+        }).then((reciept)=>{
+            return tokenInstance.approve(spendingAccount , 10 , {from :fromAccount})
+        }).then((reciept)=>{
+            return tokenInstance.transferFrom(fromAccount , toAccount , 99999 , {from :spendingAccount});
+        }).then(assert.fail).catch(e=>{
+            assert(e.message.toString().indexOf('revert')>=0 , "cannot transfer value larger than balance")
+
+            return tokenInstance.transferFrom(fromAccount , toAccount , 20 , {from:spendingAccount})
+        }).then(assert.fail).catch(e=>{
+            assert(e.message.toString().indexOf('revert')>=0 , "cannot transfer value larger than approved amount" )
+            return tokenInstance.transferFrom.call(fromAccount , toAccount , 10 , {from:spendingAccount})
+        }).then((reciept)=>{
+            assert.equal(reciept , true , "delegated transfer token occured sucessfully")
+            return tokenInstance.transferFrom(fromAccount , toAccount , 10 , {from:spendingAccount})
+
+        }).then((reciept)=>{
+            assert(reciept.logs.length,1,'trigger one event')
+            assert(reciept.logs[0].event,'Transfer','trigger Transfer event')
+            assert(reciept.logs[0].args._from,accounts[0],'logs the account the tokens are transfered from')
+            assert(reciept.logs[0].args._to,accounts[1],'logs the account the tokens are transfered to')
+            assert(reciept.logs[0].args._value,10,'logs the  Transfer amount')
+            assert(tokenInstance.balance_of(toAccount) , 10 , 'adds the amount to recieved account')
+            return tokenInstance.balance_of(fromAccount);
+        }).then((balance)=>{
+            assert.equal(Number(balance) , 90 , 'token deducted from fromAccount')
+            return tokenInstance.balance_of(toAccount)
+        }).then((balance)=>{
+            assert.equal(Number(balance) , 10 , 'tokens added to toAccount')
+            return tokenInstance.allowance(fromAccount , spendingAccount);
+        }).then((balance)=>{
+            assert.equal(Number(balance) , 0 , 'spenders allocated tokens have been deducted')
+        })
+    })
 })
