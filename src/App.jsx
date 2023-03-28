5aa8f6ccc447ab8c16ba27f1fe6ddabb68973e79
DeepakSingh260 <alexmha260@gmail.com>
2023-03-28 12:17:39 +0530

    frontend setup with vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        <h1>Deep Token Sale</h1>
+    </div>
+  )
+}
+
+export default App
